438823699742c803f0a9b6195501b9f9c50ae403
Anagha <55786598+chakkara@users.noreply.github.com>
2020-12-22 22:04:55 +0530

    Add files via upload
    final1

--- urls.py
+++ urls.py
@@ -1,19 +1,24 @@
-from django.urls import path
-from . import views
+"""disease_prediction URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
 
 urlpatterns = [
-
-
-    path('sign_in_admin', views.sign_in_admin , name='sign_in_admin'),
-
-    path('signup_patient', views.signup_patient, name="signup_patient"),
-    path('sign_in_patient', views.sign_in_patient , name='sign_in_patient'),
-    path('savepdata/<str:patientusername>', views.savepdata , name='savepdata'),
-    
-
-    path('signup_doctor', views.signup_doctor , name="signup_doctor"),
-    path('sign_in_doctor', views.sign_in_doctor , name='sign_in_doctor'),
-    path('saveddata/<str:doctorusername>', views.saveddata , name='saveddata'),
-
-    path('logout', views.logout , name='logout'),
+     path('admin/', admin.site.urls),
+     path("", include("main_app.urls")),
+     path("accounts/", include("accounts.urls")),
+     path("", include("chats.urls"))
 ]
